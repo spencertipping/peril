3cf67a2f489e5a8861778f9b6cb61c6d3b252104
Spencer Tipping <spencer.tipping@gmail.com>
2017-08-13 05:36:00 -0600

    Removed no, which is now a separate project

--- dev/hackery/replace-grep.pl
+++
@@ -1,7 +0,0 @@
-use strict;
-use warnings;
-
-BEGIN { *grep = sub(&@) { print "you've grepped something\n" } }
-
-# aha, grep is too magical to be replaced
-my @xs = grep {$_ & 1} 1, 2, 3;
--- dev/hackery/use-no.pl
+++
@@ -1,9 +0,0 @@
-use strict;
-use warnings;
-
-BEGIN { ++$INC{'no.pm'} }
-sub no::import   { print "using no\n" }
-sub no::unimport { print "noing no\n" }
-
-use no;
-no no;
